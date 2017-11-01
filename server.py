3ae2943298b0c8b806a67a86ce27c906fe712e2b
focusheart <focusheart@qq.com>
2017-11-01 18:46:35 +0800

    basic structure

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
 # -*- coding: utf8 -*-
+'''
+Simple Write MD Server Program
 
-from flask import Flask
-from flask import render_template
+Provide list, query, edit, save, etc
+
+'''
+
 import os
 from ConfigParser import ConfigParser
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+from flask import request
+from flask import abort
 
 config = ConfigParser()
 config.read('config.ini')
@@ -14,15 +23,39 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index'
+    '''
+    Index page of this tiny site
+    '''
+    return render_template('index.html')
+
+@app.route('/list')
+def list_files():
+    '''
+    List files in specified path
+    '''
+    path = request.args.get('p')
+    if path is None or path.strip() == '':
+        return abort(404)
+
+    # get file list here
+
+    return render_template('list.html')
 
 @app.route('/edit')
 def edit():
-    return 'edit'
+    '''
+    Edit specified file
+    '''
+    return render_template('edit.html')
 
 @app.route('/save', methods=['GET', 'POST'])
 def save():
-    return 'save'
+    '''
+    Save submitted md content
+    '''
+    # Handle the save file operation
+    ret = {'success': True}
+    return jsonify(ret)
 
 if __name__ == '__main__':
     app.run()
